6e6ab78d095a21f4b2a8d4266d3db42b6e9cea88
natalia <natkapral@gmail.com>
2014-09-29 21:45:54 +0100

    git rid of apc cache

--- web/app.php
+++ web/app.php
@@ -5,8 +5,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-$loader = new ApcClassLoader('sylius', $loader);
-$loader->register(true);
+//$loader = new ApcClassLoader('sylius', $loader);
+//$loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
