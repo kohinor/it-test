02ff4425dcea614cb9028c8a3eeac0b1e0c62781
natalia <natkapral@gmail.com>
2014-09-29 21:36:10 +0100

    bootstrap

--- web/app.php
+++ web/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
+//use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
@@ -9,7 +9,7 @@ $loader = new ApcClassLoader('sylius', $loader);
 $loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $loader->register(true);
