8b5635081b1651173959431ca269a8ee90448c76
natalia.kapralova <natkapral@gmail.com>
2015-06-08 18:24:06 +0100

    updated the minified screen display

--- src/App/SyliusProductBundle/Command/UploadProductCommand.php
+++ src/App/SyliusProductBundle/Command/UploadProductCommand.php
@@ -166,7 +166,7 @@ class UploadProductCommand extends ContainerAwareCommand
             }
             $product->setTaxons($taxons);
             $attributes[] = array('name' => 'Brand', 'value' => $productDropship->getBrand());
-            if (isset($productDropship->getModels()->first()) && $productDropship->getModels()->first()->getColor()) {
+            if ($productDropship->getModels()->first() && $productDropship->getModels()->first()->getColor()) {
                 $attributes[] = array('name' => 'Color', 'value' => $productDropship->getModels()->first()->getColor());
             }
             $this->addAttributes($product, $attributes);
